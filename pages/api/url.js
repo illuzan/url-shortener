c49a27894cf9247cfe7e4d50e7ab5f1f16c2f660
Gaurav Singh <allstargaurav@gmail.com>
2021-08-23 16:03:09 +0530

    Add API

---
+++ pages/api/url.js
@@ -0,0 +1,21 @@
+import { supabase } from '../../utils/supabase'
+import { nanoid } from 'nanoid'
+
+const url = async (req, res) => {
+  if (req.method === 'POST') {
+    // Process a POST request
+    const short = nanoid(8)
+    const { longUrl } = req.body
+    const { data, error } = await supabase
+      .from('url')
+      .insert([
+        { shortURL: short, longURL: longUrl },
+      ])
+    if (error) return res.status(401).json({ error: error.message })
+    return res.status(200).json()
+  } else {
+    res.status(405).json()
+  }
+}
+
+export default url
