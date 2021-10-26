30bfc05e0ad911562f1ffd730995c66b07ab910c
Gaurav Singh <allstargaurav@gmail.com>
2021-10-26 22:59:17 +0530

    live Version

--- pages/api/url.js
+++ pages/api/url.js
@@ -1,15 +1,17 @@
 import { supabase } from '../../utils/supabase'
 import { nanoid } from 'nanoid'
+import { sanitizeUrl } from '@braintree/sanitize-url'
 
 const url = async (req, res) => {
   if (req.method === 'POST') {
     // Process a POST request
     const short = nanoid(8)
     const { longUrl } = req.body
+    const longUrlSanitized = sanitizeUrl(longUrl)
     const { data: [{ shortURL }], error } = await supabase
       .from('url')
       .insert([
-        { shortURL: short, longURL: longUrl },
+        { shortURL: short, longURL: longUrlSanitized },
       ])
     if (error) return res.status(401).json({ error: error.message })
     return res.status(200).json(shortURL)
