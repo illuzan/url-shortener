e2c3516e2433bef201d5348015d8d9e2b1b91766
Gaurav Singh <allstargaurav@gmail.com>
2022-01-23 00:23:17 +0530

    Add tailwind again

--- app/root.tsx
+++ app/root.tsx
@@ -4,20 +4,24 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  ScrollRestoration,
+} from 'remix'
+import type { MetaFunction, LinksFunction } from 'remix'
+import styles from './styles/app.css'
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+  return { title: 'New Remix App' }
+}
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
         <Meta />
         <Links />
       </head>
@@ -25,8 +29,8 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
-  );
+  )
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,80 @@
+import { Form } from 'remix'
+import { useEffect, useState } from 'react'
 export default function Index() {
+  const [states, setStates] = useState(true)
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+    <div className='relative flex flex-col justify-center min-h-screen py-6 overflow-hidden bg-gray-50 sm:py-12'>
+      <img
+        src='/background.jpg'
+        alt=''
+        className='absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-none'
+        width='1308'
+      />
+      <div className='absolute inset-0 bg-[url(https://play.tailwindcss.com/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]'></div>
+      <div className='relative mt-[-10rem]'>
+        {/* Text */}
+        <div className='px-4 py-10 mx-auto sm:w-3/4 lg:w-2/3 sm:p-10 lg:mb-10'>
+          <h1 className='mb-6 text-6xl font-extrabold leading-none tracking-tight text-center'>
+            Create Short URL!
+          </h1>
+          <p className='justify-center mx-auto text-xl font-normal leading-7 text-center xl:w-2/4'>
+            Fast and simple website to create a shortened URL, easy to remember
+            and share.
+          </p>
+        </div>
+        {/* Input Container */}
+        <div className='px-4 py-10 mx-auto sm:w-3/4 lg:w-2/3 xl:w-2/5 sm:p-10 md:rounded glass'>
+          <Form action='#'>
+            <fieldset
+              className='py-10 sm:mx-auto sm:max-w-2xl sm:flex'
+              // disabled={true}
+            >
+              <div className='flex-1 min-w-0'>
+                <label htmlFor='long-url' className='sr-only'>
+                  Long URL
+                </label>
+                <input
+                  id='long-url'
+                  type='url'
+                  className='block w-full px-5 py-3 text-base text-gray-900 placeholder-gray-500 border border-transparent rounded-md shadow-sm disabled:bg-gray-200 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-blue-600 '
+                  placeholder='https://www.example.com/'
+                />
+              </div>
+              <div className='mt-4 sm:mt-0 sm:ml-3'>
+                <button
+                  type='submit'
+                  onClick={() => setStates(!states)}
+                  className='block w-full px-5 py-3 text-base font-medium text-white bg-blue-500 border border-transparent rounded-md shadow disabled:bg-blue-400 sm:w-48 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-600 sm:px-10'
+                >
+                  {states === true ? 'Creating...' : 'Get Short URL'}
+                </button>
+              </div>
+            </fieldset>
+            <p id='error-message'>
+              {states === 'error' ? actionData.message : <>&nbsp;</>}
+            </p>
+          </Form>
+
+          {/* <Form
+          method='post'
+          className='max-w-md py-10 mx-auto sm:max-w-2xl md:max-w-3xl md:flex'
+        >
+          <input
+            className='w-full px-4 py-3 mb-2 text-gray-800 border-2 border-blue-400 rounded bg-gray-50 focus:outline-none md:border-r-0 focus:bg-blue-50 md:rounded-r-none md:mb-0'
+            type='url'
+            name='longUrl'
+            placeholder='https://www.example.com/'
+          />
+          <button
+            type='submit'
+            className='w-full px-4 py-3 text-blue-100 bg-blue-600 rounded-r cursor-pointer disabled:opacity-50 disabled:hover:bg-blue-600 hover:bg-blue-700 md:w-32 focus:outline-none focus:ring-2'
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+            Get Short
+          </button>
+
+        </Form> */}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
