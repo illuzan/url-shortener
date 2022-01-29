e3662148816146aeb763ffda0af6d2f11dde8481
Gaurav Singh <allstargaurav@gmail.com>
2022-01-30 01:31:24 +0530

    SEO

--- app/root.tsx
+++ app/root.tsx
@@ -10,10 +10,25 @@ import type { MetaFunction, LinksFunction } from 'remix'
 import styles from './styles/app.css'
 
 export const meta: MetaFunction = () => {
-  return { title: 'New Remix App' }
+  return {
+    title: 'Little URL',
+    description: 'Create short url from long url in one click',
+    'og:type': 'website',
+    'og:title': 'Little URL',
+    'og:url': 'https://www.littleurl.online/',
+    'og:image': 'https://i.imgur.com/vn5Bsp9.png',
+    'og:description': 'Create short url from long url in one click',
+  }
 }
 export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: styles }]
+  return [
+    {
+      rel: 'icon',
+      href: '/favicon.png',
+      type: 'image/png',
+    },
+    { rel: 'stylesheet', href: styles },
+  ]
 }
 
 export default function App() {
